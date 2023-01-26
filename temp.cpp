7a6b992135364137a5565584e4af366cf388c937
Riaj509 <mehedial00@gmail.com>
2023-01-26 20:58:53 +0600

    lol

---
+++ temp.cpp
@@ -0,0 +1,173 @@
+//....................................<In the name of Allah>...............................//
+
+//.................................<Bismillahir Rahmanir Rahim>...................................//
+#include <bits/stdc++.h>
+using namespace std;
+
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+
+typedef tree<long long, null_type, less_equal<long long>, rb_tree_tag, tree_order_statistics_node_update> ordered_set;
+
+typedef long long ll;
+typedef vector<ll> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<ll, ll> pii;
+typedef pair<double, double> pdd;
+typedef vector<pii> vii;
+typedef vector<pair<ll, ll>> vli;
+typedef double dl;
+typedef map<ll, ll> mp;
+
+#define lower(a, b) lower_bound((a).begin(), (a).end(), b) - (a).begin()
+#define mem(a, b) memset(a, b, sizeof(a));
+#define sz1 cout << endl;
+#define pb push_back
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define MOD 1000000007
+
+#define infll 0n7f7f7f7f7f7f7f7f
+#define fraction(a)               \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(a);            \
+    cout.setf(ios::fined, ios::floatfield);
+//////////////////////////////Debug part////////////////////////////////////
+
+template <typename F, typename S>
+ostream &operator<<(ostream &os, const pair<F, S> &p)
+{
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    os << "{";
+    for (auto it = v.begin(); it != v.end(); ++it)
+    {
+        if (it != v.begin())
+            os << ", ";
+        os << *it;
+    }
+    return os << "}";
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const set<T> &v)
+{
+    os << "[";
+    for (auto it = v.begin(); it != v.end(); ++it)
+    {
+        if (it != v.begin())
+            os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const multiset<T> &v)
+{
+    os << "[";
+    for (auto it = v.begin(); it != v.end(); ++it)
+    {
+        if (it != v.begin())
+            os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+template <typename F, typename S>
+ostream &operator<<(ostream &os, const map<F, S> &v)
+{
+    os << "[";
+    for (auto it = v.begin(); it != v.end(); ++it)
+    {
+        if (it != v.begin())
+            os << ", ";
+        os << it->first << " = " << it->second;
+    }
+    return os << "]";
+}
+
+#define dbg(args...)            \
+    do                          \
+    {                           \
+        cerr << #args << " : "; \
+        faltu(args);            \
+    } while (0)
+
+void faltu()
+{
+    cerr << endl;
+}
+
+template <typename T>
+void faltu(T a[], ll n)
+{
+    for (ll i = 0; i < n; ++i)
+        cerr << a[i] << ' ';
+    cerr << endl;
+}
+
+template <typename T, typename... hello>
+void faltu(T arg, const hello &...rest)
+{
+    cerr << arg << ' ';
+    faltu(rest...);
+}
+
+//////////////////////////////-------->
+
+bool isKthBitSet(int n, int k)
+{
+    if (n & (1 << (k - 1)))
+        return true;
+    return false;
+}
+bool poweroftwo(int k) // complexity O(1)
+{
+    return k && (!(k & (k - 1)));
+}
+bool powerofthree(int n) // complexity O(1)
+{
+    if (n <= 0)
+        return false;
+    return 1162261467 % n == 0;
+}
+
+////////powMod---custom//////
+ll power(ll x, ll y, ll p)
+{
+    ll res = 1;
+
+    x = x % p;
+
+    while (y > 0)
+    {
+
+        if (y & 1)
+            res = (res * x) % p;
+
+        y = y >> 1;
+        x = (x * x) % p;
+    }
+    return res;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int tc = 1;
+    cin >> tc;
+    while (tc--)
+    {
+    }
+
+    return 0;
+}
