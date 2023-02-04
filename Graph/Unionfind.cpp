60e33b4d4c93e7e659b2b9200520c9122614e81b
Riaj509 <mehedial00@gmail.com>
2023-02-04 20:06:06 +0600

    lol

--- Graph/Unionfind.cpp
+++ Graph/Unionfind.cpp
@@ -4,43 +4,69 @@ typedef long long int ll;
 #define all(a) (a).begin(), (a).end()
 #define rall(a) (a).rbegin(), (a).rend()
 
-const int mxn = 1e5;
+const ll N = 3e5 + 9;
+
 struct DSU
 {
-    int parent[mxn];
-    int sz[mxn];
-
-    void init(int n)
+    vector<ll> par, rnk, sz;
+    ll c;
+    DSU(ll n) : par(n + 1), rnk(n + 1, 0), sz(n + 1, 1), c(n)
     {
-        for (int i = 1; i <= n; i++)
-        {
-            parent[i] = i;
-            sz[i] = 1;
-        }
+        for (ll i = 1; i <= n; ++i)
+            par[i] = i;
     }
-    int findrep(int u)
+    ll find(ll i)
     {
-        return parent[u] == u ? u : parent[u] = findrep(parent[u]);
+        return (par[i] == i ? i : (par[i] = find(par[i])));
     }
-    void unite(int u, int v)
+    bool same(ll i, ll j)
     {
-        u = findrep(u);
-        v = findrep(v);
-        if (u == v)
-            return;
-
-        if (sz[u] > sz[v])
-            swap(u, v);
-        parent[u] = v;
-        sz[v] += sz[u];
+        return find(i) == find(j);
     }
-
-} dsu;
-
+    ll get_size(ll i)
+    {
+        return sz[find(i)];
+    }
+    ll count()
+    {
+        return c; // connected components
+    }
+    ll merge(ll i, ll j)
+    {
+        if ((i = find(i)) == (j = find(j)))
+            return -1;
+        else
+            --c;
+        if (rnk[i] > rnk[j])
+            swap(i, j);
+        par[i] = j;
+        sz[j] += sz[i];
+        if (rnk[i] == rnk[j])
+            rnk[j]++;
+        return j;
+    }
+};
 int main()
 {
 
-    ll n;
-    cin >> n;
-    dsu.init(n);
+    ll n, m, cnt = 0;
+    cin >> n >> m;
+    DSU ds(n);
+    vector<pair<ll, ll>> v(m);
+    for (ll i = 0; i < m; i++)
+    {
+        cin >> v[i].first >> v[i].second;
+    }
+    sort(all(v));
+
+    for (ll i = 0; i < m; i++)
+    {
+        ll x, y;
+        x = v[i].first, y = v[i].second;
+        if (ds.same(x, y))
+            cnt++;
+        else
+            ds.merge(x, y);
+    }
+    cout << cnt << " " << ds.count() << "\n";
 }
