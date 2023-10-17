d8f2c65d9588ba1a8a783b96691208092f9760b2
Riaj509 <mehedial00@gmail.com>
2023-10-17 12:42:53 +0600

    upd

---
+++ cses/String/Distinct Substrings.cpp
@@ -0,0 +1,141 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long int ll;
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+
+const int maxN = 1e5 + 5;
+
+struct Node
+{
+    ll dp;
+    int len, link, cnt, firstpos;
+    map<char, int> nxt;
+} node[2 * maxN];
+
+string S, T;
+int N, M, K, sz, last;
+
+void extend(char c)
+{
+    int cur = sz++;
+    node[cur].len = node[last].len + 1;
+    node[cur].cnt = 1;
+    node[cur].firstpos = node[cur].len - 1;
+    int p = last;
+    while (p != -1 && !node[p].nxt.count(c))
+    {
+        node[p].nxt[c] = cur;
+        p = node[p].link;
+    }
+    if (p == -1)
+    {
+        node[cur].link = 0;
+    }
+    else
+    {
+        int q = node[p].nxt[c];
+        if (node[p].len + 1 == node[q].len)
+        {
+            node[cur].link = q;
+        }
+        else
+        {
+            int clone = sz++;
+            node[clone].len = node[p].len + 1;
+            node[clone].nxt = node[q].nxt;
+            node[clone].link = node[q].link;
+            node[clone].firstpos = node[q].firstpos;
+            while (p != -1 && node[p].nxt[c] == q)
+            {
+                node[p].nxt[c] = clone;
+                p = node[p].link;
+            }
+            node[q].link = node[cur].link = clone;
+        }
+    }
+    last = cur;
+}
+
+void init()
+{
+    node[0].len = 0;
+    node[0].link = -1;
+    sz = 1;
+    last = 0;
+}
+
+void update_cnt()
+{
+    vector<int> state_sz[sz];
+    for (int i = 0; i < sz; i++)
+        state_sz[node[i].len].push_back(i);
+    for (int i = sz - 1; i >= 0; i--)
+    {
+        for (auto u : state_sz[i])
+        {
+            if (node[u].link != -1)
+                node[node[u].link].cnt += node[u].cnt;
+        }
+    }
+}
+int query_fidx()
+{
+    int u = 0;
+    for (int i = 0; i < M; i++)
+    {
+        char c = T[i];
+        if (!node[u].nxt.count(c))
+            return -1;
+        else
+            u = node[u].nxt[c];
+    }
+    return node[u].firstpos - M + 2;
+}
+
+void distnt_substr(int u = 0)
+{
+    node[u].dp = 1;
+    for (const auto v : node[u].nxt)
+    {
+        if (!node[v.second].dp)
+            distnt_substr(v.second);
+        node[u].dp += node[v.second].dp;
+    }
+}
+
+int query_cnt()
+{
+    int u = 0;
+    for (int i = 0; i < M; i++)
+    {
+        char c = T[i];
+        if (!node[u].nxt.count(c))
+            return 0;
+        else
+            u = node[u].nxt[c];
+    }
+    return node[u].cnt;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll tc = 1;
+    // cin >> tc;
+    while (tc--)
+    {
+        cin >> S;
+        N = S.size();
+        init();
+
+        for (int i = 0; i < N; i++)
+            extend(S[i]);
+        // update_cnt();
+        distnt_substr();
+        cout << node[0].dp - 1 << "\n";
+    }
+}
